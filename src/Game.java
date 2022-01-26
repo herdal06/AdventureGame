ce1646ab693a0c70a0059c855a381aa140e8d471
herdal06 <h.erdal721@gmail.com>
2022-01-26 15:13:05 +0300

    First Commit.

---
+++ src/Game.java
@@ -0,0 +1,4 @@
+public class Game {
+    Player player;
+    Location location;
+}
---
+++ src/Player.java
@@ -0,0 +1,60 @@
+public class Player {
+    private String name;
+    private String charName;
+    private int damage;
+    private int health;
+    private int money;
+    Inventory inventory;
+
+    public Player(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCharName() {
+        return charName;
+    }
+
+    public void setCharName(String charName) {
+        this.charName = charName;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public int getMoney() {
+        return money;
+    }
+
+    public void setMoney(int money) {
+        this.money = money;
+    }
+
+    public Inventory getInventory() {
+        return inventory;
+    }
+
+    public void setInventory(Inventory inventory) {
+        this.inventory = inventory;
+    }
+}
