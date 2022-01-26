60ec79bc37140665a48dd3b53237f66bc507cf51
herdal06 <h.erdal721@gmail.com>
2022-01-26 15:36:14 +0300

    character choose menu updated. character stats added.

--- src/Player.java
+++ src/Player.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Player {
     private String name;
     private String charName;
@@ -6,19 +8,59 @@ public class Player {
     private int money;
     private Inventory inventory;
 
+    Scanner scanner = new Scanner(System.in);
+
     public Player(String name) {
         this.name = name;
     }
 
     public void selectChar() {
-        charMenu();
+        switch (charMenu()) {
+            // samurai selected
+            case 1:
+                setCharName("Samurai");
+                setDamage(5);
+                setHealth(21);
+                setMoney(15);
+                break;
+            // archer selected
+            case 2:
+                setCharName("Archer");
+                setDamage(7);
+                setHealth(18);
+                setMoney(20);
+                break;
+            // knight selected
+            case 3:
+                setCharName("Knight");
+                setDamage(8);
+                setHealth(24);
+                setMoney(5);
+                break;
+                // samurai is default character
+            default:
+                setCharName("Samurai");
+                setDamage(5);
+                setHealth(21);
+                setMoney(15);
+                break;
+        }
+        System.out.println("Your  character has been created.");
+        System.out.println("Character : "+getCharName()+", Damage : "+getDamage()+", Health : "+getHealth()+", Money : "+getMoney());
     }
 
-    public void charMenu() {
-        System.out.println("Please choose a character");
+    public int charMenu() {
+        System.out.println("Choose a character");
         System.out.println("1. Samurai \t Damage : 5 \t Health : 21 \t Money : 15");
         System.out.println("2. Archer \t Damage : 7 \t Health : 18 \t Money : 20");
         System.out.println("3. Knight \t Damage : 8 \t Health : 24 \t Money : 5");
+        System.out.println("What is your choose ?");
+        int charID = scanner.nextInt();
+        while(charID < 1 || charID > 3) {
+            System.out.println("Please select a valid character.");
+            charID = scanner.nextInt();
+        }
+        return charID;
     }
 
     public String getName() {
